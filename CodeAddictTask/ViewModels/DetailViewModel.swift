7f91168fbb1b5d3445bb71fa8079b9a8118fb052
Jakub <jh.homik@gmail.com>
2020-12-20 17:46:21 +0100

    add viewModels

---
+++ CodeAddictTask/ViewModels/DetailViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  DetailViewModel.swift
+//  CodeAddictTask
+//
+//  Created by Jakub Homik on 17/12/2020.
+//
+
+import Foundation
+
+protocol DetailUpdateDelegate: AnyObject {
+    func showloadingSpinner()
+    func hideLoadingSpinner()
+}
+
+protocol ReloadDetailTableViewDelegate: AnyObject {
+    func reloadTableView()
+}
+
+protocol UpdateDetailViewDelegate: AnyObject {
+    func updateUI(with details: DetailRepositories)
+}
+
+class DetailViewModel {
+    
+    private let networkManager = NetworkManager()
+    weak var delegate: DetailUpdateDelegate?
+    weak var reloadTableView: ReloadDetailTableViewDelegate?
+//    private let detailView = DetailView()
+    
+    var repositoryTitle: Repositories? {
+        didSet {
+//            downloadRepositories()
+            downloadCommits()
+        }
+    }
+    
+    private(set) var detailRepositories: DetailRepositories? {
+        didSet {
+//            detailView.detailRepositories = detailRepositories
+        }
+    }
+    
+    private(set) var listCommits: ListCommits = [] {
+        didSet {
+            reloadTableView?.reloadTableView()
+        }
+    }
+    
+    func downloadRepositories() {
+        delegate?.showloadingSpinner()
+        networkManager.getRepositories(forOwner: repositoryTitle?.owner.login ?? "", repoName: repositoryTitle?.name ?? "") { [weak self] (result) in
+            guard let self = self else { return }
+            self.delegate?.hideLoadingSpinner()
+            switch result {
+            case .success(let detailRepositories):
+                DispatchQueue.main.async {
+                    self.detailRepositories = detailRepositories
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func downloadCommits() {
+        networkManager.getListCommits(forOwner: repositoryTitle?.owner.login ?? "" , repoName: repositoryTitle?.name ?? "") { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let commits):
+                DispatchQueue.main.async {
+                    self.listCommits = commits
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
---
+++ CodeAddictTask/ViewModels/MainViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  MainViewModel.swift
+//  CodeAddictTask
+//
+//  Created by Jakub Homik on 17/12/2020.
+//
+
+import Foundation
+
+protocol MainUpdateDelegate: AnyObject {
+    func showLoadingSpinner()
+    func hideLoadingSpinner()
+    func pushDetailViewControler(with repository: Repositories)
+}
+
+protocol ReloadMainTableViewDelegate: AnyObject {
+    func reloadTableView()
+}
+
+class MainViewModel {
+    
+    private let networkManager = NetworkManager()
+    weak var delegate: MainUpdateDelegate?
+    weak var updateRepositories: ReloadMainTableViewDelegate?
+
+    private(set) var currentPage = 1 {
+        didSet {
+            searchForRepositories()
+        }
+    }
+    
+    private(set) var searchWord = String() {
+        didSet {
+            searchForRepositories()
+        }
+    }
+    
+    private(set) var filteredRepositories: [Repositories] = [] {
+        didSet {
+            updateRepositories?.reloadTableView()
+        }
+    }
+    
+    func cancelButtonTapped() {
+        filteredRepositories.removeAll()
+    }
+    
+    func checkSearchBar(_ word: String?) {
+        guard let query = word, query.trimmingCharacters(in: .whitespaces) != "" else { return filteredRepositories.removeAll()
+                }
+        print(query)
+    }
+    
+    func currentIndexPath(_ indexPath: IndexPath) {
+        if indexPath.section == currentPage * 30 - 2 {
+            currentPage += 1
+        }
+    }
+    
+    func updateSearchWord(_ searchForWord: String?) {
+        filteredRepositories.removeAll()
+        guard let word = searchForWord else { return }
+        searchWord = word
+    }
+    
+    func rowSelectedAt(indexPath: IndexPath) {
+        delegate?.pushDetailViewControler(with: filteredRepositories[indexPath.section])
+    }
+    
+    func searchForRepositories() {
+        delegate?.showLoadingSpinner()
+        networkManager.searchRepositories(withWord: searchWord, page: currentPage) { [weak self] (result) in
+            guard let self = self else { return }
+            self.delegate?.hideLoadingSpinner()
+            switch result {
+            case .success(let repositories):
+                DispatchQueue.main.async {
+                    if self.filteredRepositories.isEmpty {
+                        self.filteredRepositories = repositories.items
+                    } else {
+                        self.filteredRepositories.append(contentsOf: repositories.items)
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
