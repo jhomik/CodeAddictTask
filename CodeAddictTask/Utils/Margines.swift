03863735c249c4a3959d6030a66bef7de247a743
Jakub <jh.homik@gmail.com>
2021-01-27 08:51:44 +0100

    add Margines and Fonts constants to project

---
+++ CodeAddictTask/Utils/Margines.swift
@@ -0,0 +1,120 @@
+//
+//  Margines.swift
+//  CodeAddictTask
+//
+//  Created by Jakub Homik on 27/01/2021.
+//
+
+import UIKit
+
+struct Margines {
+    static let searchRepositoriesTableViewLeading: CGFloat = 16
+    static let searchRepositoriesTableViewTrailing: CGFloat = -16
+    static let searchRepositoriesTableViewRowHeight: CGFloat = 92
+    static let searchRepositoriesTableViewSectionHeightHeader: CGFloat = 66
+    static let searchRepositoriesTableViewSectionDefaultHeight: CGFloat = 9
+    
+    static let detailViewUserAvatarImageViewTop: CGFloat = -4
+    static let detailViewUserAvatarImageViewHeight: CGFloat = 263
+    
+    static let repositoryByLabelTop: CGFloat = 159
+    static let repositoryByLabelLeading: CGFloat = 20
+    static let repositoryByLabelTrailing: CGFloat = -20
+    static let repositoryByLabelHeight: CGFloat = 20
+    static let repositoryByLabelAlpha: CGFloat = 0.6
+    
+    static let repoAuthorNameLabelTop: CGFloat = 4
+    static let repoAuthorNameLabelHeight: CGFloat = 34
+    
+    static let repositoryStarFillImageViewTop: CGFloat = 9
+    static let repositoryStarFillImageViewWidth: CGFloat = 13
+    static let repositoryStarFillImageViewHeight: CGFloat = 13
+    
+    static let numberOfStarsLabelTop: CGFloat = 6
+    static let numberOfStarsLabelLeading: CGFloat = 5
+    static let numberOfStarsLabelHeight: CGFloat = 18
+    static let numberOfStarsLabelAlpha: CGFloat = 0.5
+    
+    static let viewOnlineButtonTop: CGFloat = 17
+    static let viewOnlineButtonTrailing: CGFloat = -16
+    static let viewOnlineButtonWidth: CGFloat = 118
+    static let viewOnlineButtonHeight: CGFloat = 30
+    static let viewOnlineButtonCornerRadius: CGFloat = 17
+    
+    static let repositoryTitleLabelTop: CGFloat = 21
+    static let repositoryTitleLabelTrailing: CGFloat = -15
+    static let repositoryTitleLabelHeight: CGFloat = 22
+    
+    static let commitsHistoryLabelTop: CGFloat = 35
+    static let commitsHistoryLabelLeading: CGFloat = 16
+    static let commitsHistoryLabelHeight: CGFloat = 28
+    
+    static let detailShareRepoViewHeight: CGFloat = 94
+    
+    static let detailTableViewEstimatedRowHeight: CGFloat = 111
+    static let detailTableViewBottom: CGFloat = -24
+    
+    static let shareRepoButtonBottom: CGFloat = -44
+    static let shareRepoButtonLeading: CGFloat = 16
+    static let shareRepoButtonTrailing: CGFloat = -16
+    static let shareRepoButtonHeight: CGFloat = 50
+    
+    static let shareRepoImageViewLeading: CGFloat = 113
+    static let shareRepoImageViewHeight: CGFloat = 18
+    static let shareRepoImageViewWidth: CGFloat = 18
+    
+    static let shareRepoLabelLeading: CGFloat = 9
+    static let shareRepoLabelHeight: CGFloat = 22
+    static let shareRepoLabelWidth: CGFloat = 92
+    
+    static let searchRepositoriesUserAvatarImageViewCornerRadius: CGFloat = 10
+    static let searchRepositoriesUserAvatarImageViewTop: CGFloat = 16
+    static let searchRepositoriesUserAvatarImageViewLeading: CGFloat = 16
+    static let searchRepositoriesUserAvatarImageViewWidth: CGFloat = 60
+    static let searchRepositoriesUserAvatarImageViewHeight: CGFloat = 60
+    
+    static let searchRepositoriesCustomCellCornerRadius: CGFloat = 13
+    
+    static let userRepositoryTitleLabelTop: CGFloat = 10
+    static let userRepositoryTitleLabelLeading: CGFloat = 16
+    static let userRepositoryTitleLabelWidth: CGFloat = 207
+    static let userRepositoryTitleLabelHeight: CGFloat = 22
+    
+    static let repositoryStarImageViewTop: CGFloat = 4
+    static let repositoryStarImageViewLeading: CGFloat = 16
+    static let repositoryStarImageViewHeight: CGFloat = 14
+    static let repositoryStarImageViewWidth: CGFloat = 14
+    
+    static let numberOfRepositoryStarsLabelLeading: CGFloat = 4
+    static let numberOfRepositoryStarsLabelTrailing: CGFloat = 16
+    static let numberOfRepositoryStarsLabelHeight: CGFloat = 22
+    
+    static let disclosureIndicatorImageViewTop: CGFloat = 39
+    static let disclosureIndicatorImageViewTrailing: CGFloat = -16
+    static let disclosureIndicatorImageViewWidth: CGFloat = 8
+    static let disclosureIndicatorImageViewHeight: CGFloat = 13
+    
+    static let circleWithNumberViewTop: CGFloat = 26
+    static let circleWithNumberViewLeading: CGFloat = 20
+    static let circleWithNumberViewWidth: CGFloat = 36
+    static let circleWithNumberViewHeight: CGFloat = 36
+    
+    static let commitAuthorNameLabelTop: CGFloat = 16
+    static let commitAuthorNameLabelLeading: CGFloat = 20
+    static let commitAuthorNameLabelTrailing: CGFloat = -16
+    static let commitAuthorNameLabelHeight: CGFloat = 13
+    
+    static let emailLabelTop: CGFloat = 2
+    static let emailLabelLeading: CGFloat = 20
+    static let emailLabelTrailing: CGFloat = -16
+    static let emailLabelHeight: CGFloat = 22
+    
+    static let commitMessageLabelTop: CGFloat = 2
+    static let commitMessageLabelLeading: CGFloat = 20
+    static let commitMessageLabelTrailing: CGFloat = -16
+    static let commitMessageLabelBottom: CGFloat = -12
+    static let commitMessageLabelHeight: CGFloat = 44
+    
+    static let shareRepoButtonCornerRadius: CGFloat = 10
+    
+}
